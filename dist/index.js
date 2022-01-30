7637831bc29b1f91c7ec28a8841ad893e944f473
Kazuo Yagi <kazuo.yagi@gmail.com>
2022-01-30 11:02:55 +0900

    Update misc

--- dist/index.js
+++ dist/index.js
@@ -8483,7 +8483,7 @@ const { pull_request } = context.payload;
   ...context.repo,
   issue_number: pull_request.number,
   body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-});})
+});})()
 
 })();
 
--- src/action.js
+++ src/action.js
@@ -13,4 +13,4 @@ const { pull_request } = context.payload;
   ...context.repo,
   issue_number: pull_request.number,
   body: 'Thank you for submitting a pull request! We will try to review this as soon as we can.'
-});})
+});})()
